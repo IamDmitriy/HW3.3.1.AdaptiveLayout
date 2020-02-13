099162bc1c8ee4306681680e23ade43729eaddc4
Dmitriy25 <ohapkin_da@mail.ru>
2020-02-13 16:06:40 +0300

    Initial commit

---
+++ app/src/main/java/com/example/adaptivelayout/MainActivity.java
@@ -0,0 +1,93 @@
+package com.example.adaptivelayout;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btnZero;
+    private Button btnOne;
+    private Button btnTwo;
+    private Button btnThree;
+    private Button btnFour;
+    private Button btnFive;
+    private Button btnSix;
+    private Button btnSeven;
+    private Button btnEigth;
+    private Button btnNine;
+    private Button btnPoint;
+    private TextView txtOutput;
+    private String textBuffer;
+    View.OnClickListener onButtonClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Button btn = (Button) v;
+            String curBtn = btn.getText().toString();
+
+            if (TextUtils.isEmpty(txtOutput.getText())) {
+                textBuffer ="";
+            } else {
+                textBuffer = txtOutput.getText().toString();
+            }
+
+            if (curBtn.equals(getString(R.string.btn_point)) &&
+                    textBuffer.contains(getString(R.string.btn_point))) {
+                Toast.makeText(MainActivity.this,
+                        getString(R.string.error_point_already_exist), Toast.LENGTH_SHORT).show();
+            } else {
+                textBuffer += curBtn;
+            }
+
+            txtOutput.setText(textBuffer);
+
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initView();
+    }
+
+    private void initView() {
+        btnZero = findViewById(R.id.btnZero);
+        btnOne = findViewById(R.id.btnOne);
+        btnTwo = findViewById(R.id.btnTwo);
+        btnThree = findViewById(R.id.btnThree);
+        btnFour = findViewById(R.id.btnFour);
+        btnFive = findViewById(R.id.btnFive);
+        btnSix = findViewById(R.id.btnSix);
+        btnSeven = findViewById(R.id.btnSeven);
+        btnEigth = findViewById(R.id.btnEight);
+        btnNine = findViewById(R.id.btnNine);
+        btnPoint = findViewById(R.id.btnPoint);
+
+        txtOutput = findViewById(R.id.txtOutput);
+
+        btnZero.setOnClickListener(onButtonClickListener);
+        btnOne.setOnClickListener(onButtonClickListener);
+        btnTwo.setOnClickListener(onButtonClickListener);
+        btnThree.setOnClickListener(onButtonClickListener);
+        btnFour.setOnClickListener(onButtonClickListener);
+        btnFive.setOnClickListener(onButtonClickListener);
+        btnSix.setOnClickListener(onButtonClickListener);
+        btnSeven.setOnClickListener(onButtonClickListener);
+        btnEigth.setOnClickListener(onButtonClickListener);
+        btnNine.setOnClickListener(onButtonClickListener);
+        btnPoint.setOnClickListener(onButtonClickListener);
+
+    }
+
+}
